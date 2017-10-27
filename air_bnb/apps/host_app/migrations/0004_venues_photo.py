39808b41fa3b90d4e6bef8c2136522b0a77f6912
Maheen Mansoor <mhn1211@gmail.com>
2017-10-26 19:13:04 -0500

    After reverting and getting css, pushing more files

--- air_bnb/apps/host_app/migrations/0004_venues_photo.py
+++ air_bnb/apps/host_app/migrations/0004_venues_photo.py
@@ -7,9 +7,9 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
-    dependencies = [
-        ('host_app', '0003_auto_20171025_1626'),
-    ]
+    # dependencies = [
+    #     ('host_app', '0003_auto_20171025_1626'),
+    # ]
 
     operations = [
         migrations.AddField(
